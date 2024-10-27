f5bc5e025e126223903e3c562ea65545bafa52a5
notaphplover <roberto.pintos.lopez@gmail.com>
2024-10-27 13:48:00 +0100

    feat(core): expose ClassMetadata and getClassMetadata

--- packages/container/libraries/core/src/index.ts
+++ packages/container/libraries/core/src/index.ts
@@ -1,1 +1,21 @@
-export {};
+import { getClassMetadata } from './metadata/calculations/getClassMetadata';
+import { ClassElementMetadata } from './metadata/models/ClassElementMetadata';
+import { ClassElementMetadataKind } from './metadata/models/ClassElementMetadataKind';
+import { ClassMetadata } from './metadata/models/ClassMetadata';
+import { ManagedClassElementMetadata } from './metadata/models/ManagedClassElementMetadata';
+import { MetadataName } from './metadata/models/MetadataName';
+import { MetadataTag } from './metadata/models/MetadataTag';
+import { MetadataTargetName } from './metadata/models/MetadataTargetName';
+import { UnmanagedClassElementMetadata } from './metadata/models/UnmanagedClassElementMetadata';
+
+export type {
+  ClassElementMetadata,
+  ClassMetadata,
+  ManagedClassElementMetadata,
+  MetadataName,
+  MetadataTag,
+  MetadataTargetName,
+  UnmanagedClassElementMetadata,
+};
+
+export { ClassElementMetadataKind, getClassMetadata };
