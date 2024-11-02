611f75f16a1837914f2b25e6025acfdbbeabe9a4
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-11-02 12:34:17 +0100

    feat(common): update ServiceIdentifier to allow Function

---
+++ .changeset/sour-eyes-sneeze.md
@@ -0,0 +1,5 @@
+---
+"@inversifyjs/common": minor
+---
+
+Updated `ServiceIdentifier` to allow `Function`
--- packages/container/libraries/common/src/services/models/ServiceIdentifier.ts
+++ packages/container/libraries/common/src/services/models/ServiceIdentifier.ts
@@ -3,4 +3,6 @@ import { Newable } from './Newable';
 export type ServiceIdentifier<TInstance = unknown> =
   | string
   | symbol
-  | Newable<TInstance>;
+  | Newable<TInstance>
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  | Function;
