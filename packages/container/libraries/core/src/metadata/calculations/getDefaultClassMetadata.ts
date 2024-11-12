13a424a68adec5f74a9d4dafeeac7dda24487782
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-11-12 19:21:14 +0100

    refactor(core): add getDefaultClassMetadata

---
+++ packages/container/libraries/core/src/metadata/calculations/getDefaultClassMetadata.ts
@@ -0,0 +1,12 @@
+import { ClassMetadata } from '../models/ClassMetadata';
+
+export function getDefaultClassMetadata(): ClassMetadata {
+  return {
+    constructorArguments: [],
+    lifecycle: {
+      postConstructMethodName: undefined,
+      preDestroyMethodName: undefined,
+    },
+    properties: new Map(),
+  };
+}
