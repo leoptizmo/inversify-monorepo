f7131a5836c77d3336f2b07620e4bdc4e5eec108
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-11-16 15:36:10 +0100

    test(core): add buildDefaultManagedMetadata

---
+++ packages/container/libraries/core/src/metadata/calculations/buildDefaultManagedMetadata.spec.ts
@@ -0,0 +1,41 @@
+import { beforeAll, describe, expect, it } from '@jest/globals';
+
+import { ServiceIdentifier } from '@inversifyjs/common';
+
+import { ClassElementMetadataKind } from '../models/ClassElementMetadataKind';
+import { ManagedClassElementMetadata } from '../models/ManagedClassElementMetadata';
+import { buildDefaultManagedMetadata } from './buildDefaultManagedMetadata';
+
+describe(buildDefaultManagedMetadata.name, () => {
+  let metadataKindFixture: ClassElementMetadataKind.singleInjection;
+  let serviceIdentifierFixture: ServiceIdentifier;
+
+  beforeAll(() => {
+    metadataKindFixture = ClassElementMetadataKind.singleInjection;
+    serviceIdentifierFixture = 'service-id';
+  });
+
+  describe('when called', () => {
+    let result: unknown;
+
+    beforeAll(() => {
+      result = buildDefaultManagedMetadata(
+        metadataKindFixture,
+        serviceIdentifierFixture,
+      );
+    });
+
+    it('should return ManagedClassElementMetadata', () => {
+      const expected: ManagedClassElementMetadata = {
+        kind: metadataKindFixture,
+        name: undefined,
+        optional: false,
+        tags: new Map(),
+        targetName: undefined,
+        value: serviceIdentifierFixture,
+      };
+
+      expect(result).toStrictEqual(expected);
+    });
+  });
+});
