1434fb00b8923c6b3f9cb6e12a15a354029b341b
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-11-16 17:54:01 +0100

    refactor(core): add buildDefaultMaybeClassElementMetadata

---
+++ packages/container/libraries/core/src/metadata/calculations/buildDefaultMaybeClassElementMetadata.spec.ts
@@ -0,0 +1,27 @@
+import { beforeAll, describe, expect, it } from '@jest/globals';
+
+import { MaybeClassElementMetadataKind } from '../models/MaybeClassElementMetadataKind';
+import { MaybeManagedClassElementMetadata } from '../models/MaybeManagedClassElementMetadata';
+import { buildDefaultMaybeClassElementMetadata } from './buildDefaultMaybeClassElementMetadata';
+
+describe(buildDefaultMaybeClassElementMetadata.name, () => {
+  describe('when called', () => {
+    let result: unknown;
+
+    beforeAll(() => {
+      result = buildDefaultMaybeClassElementMetadata();
+    });
+
+    it('should return MaybeManagedClassElementMetadata', () => {
+      const expected: MaybeManagedClassElementMetadata = {
+        kind: MaybeClassElementMetadataKind.unknown,
+        name: undefined,
+        optional: false,
+        tags: new Map(),
+        targetName: undefined,
+      };
+
+      expect(result).toStrictEqual(expected);
+    });
+  });
+});
---
+++ packages/container/libraries/core/src/metadata/calculations/buildDefaultMaybeClassElementMetadata.ts
@@ -0,0 +1,12 @@
+import { MaybeClassElementMetadataKind } from '../models/MaybeClassElementMetadataKind';
+import { MaybeManagedClassElementMetadata } from '../models/MaybeManagedClassElementMetadata';
+
+export function buildDefaultMaybeClassElementMetadata(): MaybeManagedClassElementMetadata {
+  return {
+    kind: MaybeClassElementMetadataKind.unknown,
+    name: undefined,
+    optional: false,
+    tags: new Map(),
+    targetName: undefined,
+  };
+}
