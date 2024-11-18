01c94f60c6bfbf55450740f87bbca67b6632372a
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-11-18 01:39:52 +0100

    refactor(core): add postContruct decorator

---
+++ packages/container/libraries/core/src/metadata/decorators/postConstruct.ts
@@ -0,0 +1,25 @@
+import { updateReflectMetadata } from '@inversifyjs/reflect-metadata-utils';
+
+import { classMetadataReflectKey } from '../../reflectMetadata/data/classMetadataReflectKey';
+import { updateMaybeClassMetadataPostConstructor } from '../actions/updateMaybeClassMetadataPostConstructor';
+import { getDefaultClassMetadata } from '../calculations/getDefaultClassMetadata';
+import { handleInjectionError } from '../calculations/handleInjectionError';
+
+export function postConstruct(): MethodDecorator {
+  return <T>(
+    target: object,
+    propertyKey: string | symbol,
+    _descriptor: TypedPropertyDescriptor<T>,
+  ): void => {
+    try {
+      updateReflectMetadata(
+        target.constructor,
+        classMetadataReflectKey,
+        getDefaultClassMetadata(),
+        updateMaybeClassMetadataPostConstructor(propertyKey),
+      );
+    } catch (error: unknown) {
+      handleInjectionError(target, propertyKey, undefined, error);
+    }
+  };
+}
