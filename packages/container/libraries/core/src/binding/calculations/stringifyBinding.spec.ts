67de94617fff98cceb9c2533f50be0715f4e5e38
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-11-25 12:59:22 +0100

    refactor(core): add stringifyBinding

---
+++ packages/container/libraries/core/src/binding/calculations/stringifyBinding.spec.ts
@@ -0,0 +1,81 @@
+import { beforeAll, describe, expect, it } from '@jest/globals';
+
+import { Binding } from '../models/Binding';
+import { bindingScopeValues } from '../models/BindingScope';
+import { bindingTypeValues } from '../models/BindingType';
+import { stringifyBinding } from './stringifyBinding';
+
+describe(stringifyBinding.name, () => {
+  describe.each<[string, Binding<unknown>, string]>([
+    [
+      'with instance type',
+      {
+        cache: { isRight: false, value: undefined },
+        id: 0,
+        implementationType: class Foo {},
+        isSatisfiedBy: () => true,
+        moduleId: undefined,
+        onActivation: {
+          isRight: false,
+          value: undefined,
+        },
+        onDeactivation: {
+          isRight: false,
+          value: undefined,
+        },
+        scope: bindingScopeValues.Singleton,
+        serviceIdentifier: 'service-id',
+        type: bindingTypeValues.Instance,
+      },
+      `[ type: "${bindingTypeValues.Instance}", serviceIdentifier: "service-id", scope: "${bindingScopeValues.Singleton}", implementationType: "Foo" ]`,
+    ],
+    [
+      'with service type',
+      {
+        id: 1,
+        isSatisfiedBy: () => true,
+        moduleId: undefined,
+        serviceIdentifier: 'service-id',
+        targetServiceIdentifier: 'target-service-id',
+        type: bindingTypeValues.ServiceRedirection,
+      },
+      `[ type: "${bindingTypeValues.ServiceRedirection}", serviceIdentifier: "service-id", redirection: "target-service-id" ]`,
+    ],
+    [
+      'with constant value type',
+      {
+        cache: { isRight: true, value: undefined },
+        id: 0,
+        isSatisfiedBy: () => true,
+        moduleId: undefined,
+        onActivation: {
+          isRight: false,
+          value: undefined,
+        },
+        onDeactivation: {
+          isRight: false,
+          value: undefined,
+        },
+        scope: bindingScopeValues.Singleton,
+        serviceIdentifier: 'service-id',
+        type: bindingTypeValues.ConstantValue,
+      },
+      `[ type: "${bindingTypeValues.ConstantValue}", serviceIdentifier: "service-id", scope: "${bindingScopeValues.Singleton}" ]`,
+    ],
+  ])(
+    'having a binding %s',
+    (_: string, binding: Binding<unknown>, expected: string) => {
+      describe('when called', () => {
+        let result: unknown;
+
+        beforeAll(() => {
+          result = stringifyBinding(binding);
+        });
+
+        it('should return expected result', () => {
+          expect(result).toBe(expected);
+        });
+      });
+    },
+  );
+});
---
+++ packages/container/libraries/core/src/binding/calculations/stringifyBinding.ts
@@ -0,0 +1,15 @@
+import { stringifyServiceIdentifier } from '@inversifyjs/common';
+
+import { Binding } from '../models/Binding';
+import { bindingTypeValues } from '../models/BindingType';
+
+export function stringifyBinding(binding: Binding<unknown>): string {
+  switch (binding.type) {
+    case bindingTypeValues.Instance:
+      return `[ type: "${binding.type}", serviceIdentifier: "${stringifyServiceIdentifier(binding.serviceIdentifier)}", scope: "${binding.scope}", implementationType: "${binding.implementationType.name}" ]`;
+    case bindingTypeValues.ServiceRedirection:
+      return `[ type: "${binding.type}", serviceIdentifier: "${stringifyServiceIdentifier(binding.serviceIdentifier)}", redirection: "${stringifyServiceIdentifier(binding.targetServiceIdentifier)}" ]`;
+    default:
+      return `[ type: "${binding.type}", serviceIdentifier: "${stringifyServiceIdentifier(binding.serviceIdentifier)}", scope: "${binding.scope}" ]`;
+  }
+}
