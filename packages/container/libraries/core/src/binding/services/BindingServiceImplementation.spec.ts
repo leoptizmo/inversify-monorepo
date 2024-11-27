130dfe55d13b7559cbf19a648a991818a23ef1e8
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-11-27 13:11:03 +0100

    refactor(core): add BindingServiceImplementation

---
+++ packages/container/libraries/core/src/binding/services/BindingServiceImplementation.spec.ts
@@ -0,0 +1,179 @@
+import { beforeAll, describe, expect, it } from '@jest/globals';
+
+import { Binding } from '../models/Binding';
+import { bindingScopeValues } from '../models/BindingScope';
+import { bindingTypeValues } from '../models/BindingType';
+import { BindingServiceImplementation } from './BindingServiceImplementation';
+
+describe(BindingServiceImplementation.name, () => {
+  describe('.get', () => {
+    let bindingFixture: Binding<unknown>;
+
+    beforeAll(() => {
+      bindingFixture = {
+        cache: {
+          isRight: false,
+          value: undefined,
+        },
+        id: 2,
+        isSatisfiedBy: (): boolean => true,
+        moduleId: 1,
+        onActivation: {
+          isRight: false,
+          value: undefined,
+        },
+        onDeactivation: {
+          isRight: false,
+          value: undefined,
+        },
+        scope: bindingScopeValues.Singleton,
+        serviceIdentifier: 'service-identifier-fixture',
+        type: bindingTypeValues.ConstantValue,
+      };
+    });
+
+    describe('when called, with existing bindings', () => {
+      let bindingServiceImplementation: BindingServiceImplementation;
+
+      let result: unknown;
+
+      beforeAll(() => {
+        bindingServiceImplementation = new BindingServiceImplementation();
+
+        bindingServiceImplementation.set(bindingFixture);
+
+        result = bindingServiceImplementation.get(
+          bindingFixture.serviceIdentifier,
+        );
+      });
+
+      it('should return Binding[]', () => {
+        expect(result).toStrictEqual([bindingFixture]);
+      });
+    });
+
+    describe('when called, with non existing bindings', () => {
+      let bindingServiceImplementation: BindingServiceImplementation;
+
+      let result: unknown;
+
+      beforeAll(() => {
+        bindingServiceImplementation = new BindingServiceImplementation();
+
+        result = bindingServiceImplementation.get(
+          bindingFixture.serviceIdentifier,
+        );
+      });
+
+      it('should return undefined', () => {
+        expect(result).toBeUndefined();
+      });
+    });
+  });
+
+  describe('.remove', () => {
+    let bindingFixture: Binding<unknown>;
+
+    beforeAll(() => {
+      bindingFixture = {
+        cache: {
+          isRight: false,
+          value: undefined,
+        },
+        id: 2,
+        isSatisfiedBy: (): boolean => true,
+        moduleId: 1,
+        onActivation: {
+          isRight: false,
+          value: undefined,
+        },
+        onDeactivation: {
+          isRight: false,
+          value: undefined,
+        },
+        scope: bindingScopeValues.Singleton,
+        serviceIdentifier: Symbol(),
+        type: bindingTypeValues.ConstantValue,
+      };
+    });
+
+    describe('when called, with existing bindings', () => {
+      let bindingServiceImplementation: BindingServiceImplementation;
+
+      beforeAll(() => {
+        bindingServiceImplementation = new BindingServiceImplementation();
+
+        bindingServiceImplementation.set(bindingFixture);
+        bindingServiceImplementation.remove(bindingFixture.serviceIdentifier);
+      });
+
+      describe('when called .get()', () => {
+        let result: unknown;
+
+        beforeAll(() => {
+          result = bindingServiceImplementation.get(
+            bindingFixture.serviceIdentifier,
+          );
+        });
+
+        it('should return undefined', () => {
+          expect(result).toBeUndefined();
+        });
+      });
+    });
+  });
+
+  describe('.removeByModule', () => {
+    let bindingFixture: Binding<unknown>;
+
+    beforeAll(() => {
+      bindingFixture = {
+        cache: {
+          isRight: false,
+          value: undefined,
+        },
+        id: 2,
+        isSatisfiedBy: (): boolean => true,
+        moduleId: 1,
+        onActivation: {
+          isRight: false,
+          value: undefined,
+        },
+        onDeactivation: {
+          isRight: false,
+          value: undefined,
+        },
+        scope: bindingScopeValues.Singleton,
+        serviceIdentifier: Symbol(),
+        type: bindingTypeValues.ConstantValue,
+      };
+    });
+
+    describe('when called, with existing bindings', () => {
+      let bindingServiceImplementation: BindingServiceImplementation;
+
+      beforeAll(() => {
+        bindingServiceImplementation = new BindingServiceImplementation();
+
+        bindingServiceImplementation.set(bindingFixture);
+        bindingServiceImplementation.removeByModule(
+          bindingFixture.moduleId as number,
+        );
+      });
+
+      describe('when called .get()', () => {
+        let result: unknown;
+
+        beforeAll(() => {
+          result = bindingServiceImplementation.get(
+            bindingFixture.serviceIdentifier,
+          );
+        });
+
+        it('should return undefined', () => {
+          expect(result).toBeUndefined();
+        });
+      });
+    });
+  });
+});
