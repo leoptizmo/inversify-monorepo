ca04b61d0b654a2ff61449fd8ba5b4a8ebcea551
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-12-03 18:55:59 +0100

    refactor: update PlanParamsConstraints with isOptional

--- packages/container/libraries/core/src/planning/models/PlanParamsConstraint.ts
+++ packages/container/libraries/core/src/planning/models/PlanParamsConstraint.ts
@@ -6,6 +6,7 @@ import { PlanParamsTagConstraint } from './PlanParamsTagConstraint';
 export interface PlanParamsConstraint {
   name?: MetadataName;
   isMultiple: boolean;
+  isOptional?: true;
   serviceIdentifier: ServiceIdentifier;
   tag?: PlanParamsTagConstraint;
 }
