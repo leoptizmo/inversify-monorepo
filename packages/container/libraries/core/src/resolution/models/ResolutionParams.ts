2a1520a0bf7759e733079d74054516ff24ff10f8
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-12-06 00:33:16 +0100

    refactor(core): update ResolutionParams with getActivations

--- packages/container/libraries/core/src/resolution/models/ResolutionParams.ts
+++ packages/container/libraries/core/src/resolution/models/ResolutionParams.ts
@@ -1,8 +1,14 @@
+import { ServiceIdentifier } from '@inversifyjs/common';
+
+import { BindingActivation } from '../../binding/models/BindingActivation';
 import { PlanResult } from '../../planning/models/PlanResult';
 import { ResolutionContext } from './ResolutionContext';
 
 export interface ResolutionParams {
   context: ResolutionContext;
+  getActivations: <TActivated>(
+    serviceIdentifier: ServiceIdentifier<TActivated>,
+  ) => BindingActivation<TActivated>[] | undefined;
   planResult: PlanResult;
   requestScopeCache: Map<number, unknown>;
 }
