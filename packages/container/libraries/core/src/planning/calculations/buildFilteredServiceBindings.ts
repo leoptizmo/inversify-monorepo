28596b2c513e30d77022d428d6421238c0fd88fa
notaphplover <roberto.pintos.lopez@gmail.com>
2024-12-13 14:12:36 +0100

    feat(core): add buildFilteredServiceBindings

---
+++ packages/container/libraries/core/src/planning/calculations/buildFilteredServiceBindings.ts
@@ -0,0 +1,32 @@
+import { ServiceIdentifier } from '@inversifyjs/common';
+
+import { Binding } from '../../binding/models/Binding';
+import {
+  BindingMetadataImplementation,
+  InternalBindingMetadata,
+} from '../../binding/models/BindingMetadataImplementation';
+import { SingleInmutableLinkedList } from '../../common/models/SingleInmutableLinkedList';
+import { BasePlanParams } from '../models/BasePlanParams';
+
+export interface BuildFilteredServiceBindingsOptions {
+  customServiceIdentifier?: ServiceIdentifier;
+}
+
+export function buildFilteredServiceBindings(
+  params: BasePlanParams,
+  bindingMetadataList: SingleInmutableLinkedList<InternalBindingMetadata>,
+  options?: BuildFilteredServiceBindingsOptions,
+): Binding<unknown>[] {
+  const bindingMetadata: BindingMetadataImplementation =
+    new BindingMetadataImplementation(bindingMetadataList.last);
+
+  const serviceIdentifier: ServiceIdentifier =
+    options?.customServiceIdentifier ?? bindingMetadata.serviceIdentifier;
+
+  const serviceBindings: Binding<unknown>[] =
+    params.getBindings(serviceIdentifier) ?? [];
+
+  return serviceBindings.filter((binding: Binding<unknown>): boolean =>
+    binding.isSatisfiedBy(bindingMetadata),
+  );
+}
