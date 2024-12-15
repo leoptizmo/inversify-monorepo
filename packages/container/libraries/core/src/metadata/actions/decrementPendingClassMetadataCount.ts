75b972d5823c7145ef064eaeb7e7f9d0f240c157
notaphplover <roberto.pintos.lopez@gmail.com>
2024-12-15 23:36:23 +0100

    refactor(core): add decrementPendingClassMetadataCount

---
+++ packages/container/libraries/core/src/metadata/actions/decrementPendingClassMetadataCount.ts
@@ -0,0 +1,24 @@
+import { updateReflectMetadata } from '@inversifyjs/reflect-metadata-utils';
+
+import { pendingClassMetadataCountReflectKey } from '../../reflectMetadata/data/pendingClassMetadataCountReflectKey';
+import { getDefaultPendingClassMetadataCount } from '../calculations/getDefaultPendingClassMetadataCount';
+import { MaybeClassElementMetadata } from '../models/MaybeClassElementMetadata';
+import { MaybeClassElementMetadataKind } from '../models/MaybeClassElementMetadataKind';
+
+export function decrementPendingClassMetadataCount(
+  type: object,
+): (metadata: MaybeClassElementMetadata | undefined) => void {
+  return (metadata: MaybeClassElementMetadata | undefined): void => {
+    if (
+      metadata !== undefined &&
+      metadata.kind === MaybeClassElementMetadataKind.unknown
+    ) {
+      updateReflectMetadata(
+        type,
+        pendingClassMetadataCountReflectKey,
+        getDefaultPendingClassMetadataCount,
+        (count: number) => count - 1,
+      );
+    }
+  };
+}
