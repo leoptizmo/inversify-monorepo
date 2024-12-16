93c84e14ebf3c1d4df978fdf4f46769c8803dcac
notaphplover <roberto.pintos.lopez@gmail.com>
2024-12-16 21:20:09 +0100

    refactor(core): add getExtendedProperties

---
+++ packages/container/libraries/core/src/metadata/calculations/getExtendedProperties.ts
@@ -0,0 +1,24 @@
+import { chain } from '../../common/calculations/chain';
+import { ClassElementMetadata } from '../models/ClassElementMetadata';
+import { ClassMetadata } from '../models/ClassMetadata';
+import { InjectFromOptions } from '../models/InjectFromOptions';
+
+export function getExtendedProperties(
+  options: InjectFromOptions,
+  baseTypeClassMetadata: ClassMetadata,
+  typeMetadata: ClassMetadata,
+): Map<string | symbol, ClassElementMetadata> {
+  const extendProperties: boolean = options.extendProperties ?? false;
+
+  let extendedProperties: Map<string | symbol, ClassElementMetadata>;
+
+  if (extendProperties) {
+    extendedProperties = new Map(
+      chain(baseTypeClassMetadata.properties, typeMetadata.properties),
+    );
+  } else {
+    extendedProperties = typeMetadata.properties;
+  }
+
+  return extendedProperties;
+}
