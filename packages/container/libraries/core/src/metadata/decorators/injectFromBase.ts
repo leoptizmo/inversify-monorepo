074b22e71bf1c069b04d8421efff9994d807bc4c
notaphplover <roberto.pintos.lopez@gmail.com>
2024-12-20 12:23:48 +0100

    refactor(core): add injectFromBase

---
+++ packages/container/libraries/core/src/metadata/decorators/injectFromBase.ts
@@ -0,0 +1,26 @@
+import { Newable } from '@inversifyjs/common';
+import { getBaseType } from '@inversifyjs/prototype-utils';
+
+import { InversifyCoreError } from '../../error/models/InversifyCoreError';
+import { InversifyCoreErrorKind } from '../../error/models/InversifyCoreErrorKind';
+import { InjectFromBaseOptions } from '../models/InjectFromBaseOptions';
+import { injectFrom } from './injectFrom';
+
+export function injectFromBase(options: InjectFromBaseOptions): ClassDecorator {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  return (target: Function): void => {
+    const baseType: Newable | undefined = getBaseType(target as Newable);
+
+    if (baseType === undefined) {
+      throw new InversifyCoreError(
+        InversifyCoreErrorKind.injectionDecoratorConflict,
+        `Expected base type for type "${target.name}", none found.`,
+      );
+    }
+
+    injectFrom({
+      ...options,
+      type: baseType,
+    })(target);
+  };
+}
