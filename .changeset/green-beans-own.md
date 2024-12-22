b974b17152e0f14bf3b0629b45a2a1e42e68f784
notaphplover <roberto.pintos.lopez@gmail.com>
2024-12-23 00:51:09 +0100

    feat(container): expose modules

---
+++ .changeset/green-beans-own.md
@@ -0,0 +1,5 @@
+---
+"@inversifyjs/container": minor
+---
+
+Added `Container`.
--- packages/container/libraries/container/src/index.ts
+++ packages/container/libraries/container/src/index.ts
@@ -1,1 +1,37 @@
-export {};
+import 'reflect-metadata';
+
+import {
+  BindInFluentSyntax,
+  BindInWhenOnFluentSyntax,
+  BindOnFluentSyntax,
+  BindToFluentSyntax,
+  BindWhenFluentSyntax,
+  BindWhenOnFluentSyntax,
+} from './binding/models/BindingFluentSyntax';
+import {
+  ContainerModule,
+  ContainerModuleLoadOptions,
+} from './container/models/ContainerModule';
+import { IsBoundOptions } from './container/models/isBoundOptions';
+import { Container, ContainerOptions } from './container/services/Container';
+import { InversifyContainerError } from './error/models/InversifyContainerError';
+import { InversifyContainerErrorKind } from './error/models/InversifyContainerErrorKind';
+
+export type {
+  BindInFluentSyntax,
+  BindInWhenOnFluentSyntax,
+  BindOnFluentSyntax,
+  BindToFluentSyntax,
+  BindWhenFluentSyntax,
+  BindWhenOnFluentSyntax,
+  ContainerModuleLoadOptions,
+  ContainerOptions,
+  IsBoundOptions,
+};
+
+export {
+  Container,
+  ContainerModule,
+  InversifyContainerError,
+  InversifyContainerErrorKind,
+};
