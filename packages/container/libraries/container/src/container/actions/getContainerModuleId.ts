ae7a2cde6254bb79f9eaba9d9e344187d4a1ae61
notaphplover <roberto.pintos.lopez@gmail.com>
2024-12-23 00:36:46 +0100

    refactor: add getContainerModuleId

---
+++ packages/container/libraries/container/src/container/actions/getContainerModuleId.ts
@@ -0,0 +1,25 @@
+import {
+  getReflectMetadata,
+  setReflectMetadata,
+  updateReflectMetadata,
+} from '@inversifyjs/reflect-metadata-utils';
+
+const ID_METADATA: string = '@inversifyjs/container/bindingId';
+
+export function getContainerModuleId(): number {
+  const bindingId: number =
+    getReflectMetadata<number>(Object, ID_METADATA) ?? 0;
+
+  if (bindingId === Number.MAX_SAFE_INTEGER) {
+    setReflectMetadata(Object, ID_METADATA, Number.MIN_SAFE_INTEGER);
+  } else {
+    updateReflectMetadata(
+      Object,
+      ID_METADATA,
+      () => bindingId,
+      (id: number) => id + 1,
+    );
+  }
+
+  return bindingId;
+}
