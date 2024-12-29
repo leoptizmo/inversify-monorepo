fec97af990850397dc0c4daecf43aeb65d669a0f
notaphplover <roberto.pintos.lopez@gmail.com>
2024-12-29 13:39:24 +0100

    feat(inversify-site): add dependency inversion page

---
+++ packages/docs/tools/inversify-code-examples/src/examples/dependencyInversion.ts
@@ -0,0 +1,32 @@
+import { Container, inject, injectable } from 'inversify';
+
+interface Weapon {
+  damage: number;
+}
+
+const weaponServiceId: symbol = Symbol.for('WeaponServiceId');
+
+@injectable()
+class Katana {
+  public readonly damage: number = 10;
+}
+
+@injectable()
+class Ninja {
+  constructor(
+    @inject(weaponServiceId)
+    public readonly weapon: Weapon,
+  ) {}
+}
+
+const container: Container = new Container();
+
+container.bind(Ninja).toSelf();
+container.bind(weaponServiceId).to(Katana);
+
+const ninja: Ninja = container.get(Ninja);
+
+console.log(ninja.weapon.damage); // Prints 10
+// End-example
+
+export { ninja };
