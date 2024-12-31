64f8a63637ba443045a3feeef9e3b771babbdb0d
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2024-12-31 01:42:05 +0100

    feat(inversify-code-examples): add getAllTagged examples

---
+++ packages/docs/tools/inversify-code-examples/src/examples/containerApiGetAllTagged.ts
@@ -0,0 +1,34 @@
+import { Container } from 'inversify';
+
+// Begin-example
+const container: Container = new Container();
+
+interface Intl {
+  hello?: string;
+  goodbye?: string;
+}
+
+container
+  .bind<Intl>('Intl')
+  .toConstantValue({ hello: 'bonjour' })
+  .whenTargetTagged('lang', 'fr');
+container
+  .bind<Intl>('Intl')
+  .toConstantValue({ goodbye: 'au revoir' })
+  .whenTargetTagged('lang', 'fr');
+
+container
+  .bind<Intl>('Intl')
+  .toConstantValue({ hello: 'hola' })
+  .whenTargetTagged('lang', 'es');
+container
+  .bind<Intl>('Intl')
+  .toConstantValue({ goodbye: 'adios' })
+  .whenTargetTagged('lang', 'es');
+
+const fr: Intl[] = container.getAllTagged<Intl>('Intl', 'lang', 'fr');
+
+const es: Intl[] = container.getAllTagged<Intl>('Intl', 'lang', 'es');
+// End-example
+
+export { es, fr };
