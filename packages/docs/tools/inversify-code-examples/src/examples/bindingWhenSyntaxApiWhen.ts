d788e9f89314f2dfc3cbbeead362cb2eda475a8f
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2025-01-02 01:22:41 +0100

    feat(inversify-code-examples): add BindingWhenSyntax when example

---
+++ packages/docs/tools/inversify-code-examples/src/examples/bindingWhenSyntaxApiWhen.ts
@@ -0,0 +1,55 @@
+import { Container, inject, injectable, interfaces, named } from 'inversify';
+
+interface Weapon {
+  damage: number;
+}
+
+export class Katana implements Weapon {
+  public readonly damage: number = 10;
+}
+
+export class Shuriken implements Weapon {
+  public readonly damage: number = 5;
+}
+
+const container: Container = new Container();
+
+// Begin-example
+const ninjaId: symbol = Symbol.for('Ninja');
+const weaponId: symbol = Symbol.for('Weapon');
+
+@injectable()
+class Ninja {
+  constructor(
+    @inject(weaponId)
+    @named('shuriken')
+    public readonly weapon: Weapon,
+  ) {}
+}
+
+container.bind<Ninja>(ninjaId).to(Ninja);
+
+const whenTargetNamedConstraint: (
+  name: string,
+) => (request: interfaces.Request) => boolean =
+  (name: string) =>
+  (request: interfaces.Request): boolean =>
+    request.target.matchesNamedTag(name);
+
+container
+  .bind<Weapon>(weaponId)
+  .to(Katana)
+  .when(whenTargetNamedConstraint('katana'));
+
+container
+  .bind<Weapon>(weaponId)
+  .to(Shuriken)
+  .when(whenTargetNamedConstraint('shuriken'));
+
+const ninja: Ninja = container.get(ninjaId);
+
+// Returns 5
+const ninjaDamage: number = ninja.weapon.damage;
+// End-example
+
+export { ninjaDamage };
