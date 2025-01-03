bcdbfc84e1b27637d88a9f30b66bc0388a88c4cc
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2025-01-03 13:07:11 +0100

    feat(inversify-code-examples): add DI hierarchy example

---
+++ packages/docs/tools/inversify-code-examples/src/examples/diHierarchy.ts
@@ -0,0 +1,16 @@
+import { Container } from 'inversify';
+
+const weaponIdentifier: symbol = Symbol.for('Weapon');
+
+// Begin-example
+class Katana {}
+
+const parentContainer: Container = new Container();
+parentContainer.bind(weaponIdentifier).to(Katana);
+
+const childContainer: Container = parentContainer.createChild();
+
+const katana: Katana = childContainer.get(weaponIdentifier);
+// End-example
+
+export { Katana, katana };
