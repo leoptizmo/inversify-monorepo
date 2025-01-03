f9d3e5149e4cb3aef2f73e15798aa763ca7db1b6
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2025-01-03 10:36:53 +0100

    feat(inversify-code-examples): add tagged decorator example

---
+++ packages/docs/tools/inversify-code-examples/src/examples/decoratorApiTagged.ts
@@ -0,0 +1,45 @@
+import { Container, inject, injectable, named, tagged } from 'inversify';
+
+interface Weapon {
+  readonly damage: number;
+}
+
+@injectable()
+export class Katana implements Weapon {
+  public readonly damage: number = 10;
+}
+
+@injectable()
+export class Shuriken implements Weapon {
+  public readonly damage: number = 5;
+}
+
+// Begin-example
+@injectable()
+class Ninja {
+  public katana: Weapon;
+  public shuriken: Weapon;
+  constructor(
+    @inject('Weapon') @tagged('weaponKind', 'melee') katana: Weapon,
+    @inject('Weapon') @tagged('weaponKind', 'ranged') shuriken: Weapon,
+  ) {
+    this.katana = katana;
+    this.shuriken = shuriken;
+  }
+}
+
+const container: Container = new Container();
+container
+  .bind<Weapon>('Weapon')
+  .to(Katana)
+  .whenTargetTagged('weaponKind', 'melee');
+container
+  .bind<Weapon>('Weapon')
+  .to(Shuriken)
+  .whenTargetTagged('weaponKind', 'ranged');
+container.bind(Ninja).toSelf();
+
+const ninja: Ninja = container.get(Ninja);
+// End-example
+
+export { ninja };
