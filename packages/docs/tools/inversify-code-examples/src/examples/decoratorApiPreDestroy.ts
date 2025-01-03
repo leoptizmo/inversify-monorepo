42f7d39d54c0aa2397e4f649fa2f17dd9bae344d
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2025-01-03 11:38:22 +0100

    feat(inversify-code-examples): add preDestroy decorator example

---
+++ packages/docs/tools/inversify-code-examples/src/examples/decoratorApiPreDestroy.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+import { jest } from '@jest/globals';
+
+import { Container, preDestroy } from 'inversify';
+
+// Begin-example
+interface Weapon {
+  damage: number;
+}
+
+export class Katana implements Weapon {
+  readonly #damage: number = 10;
+
+  public get damage(): number {
+    return this.#damage;
+  }
+
+  @preDestroy()
+  public onDeactivation(): void {
+    console.log(`Deactivating weapon with damage ${this.damage.toString()}`);
+  }
+}
+
+const container: Container = new Container();
+
+container.bind<Weapon>('Weapon').to(Katana).inSingletonScope();
+
+container.get('Weapon');
+
+// Exclude-from-example
+export const katanaDamageSpy: jest.SpiedGetter<number> = jest.spyOn<
+  Weapon,
+  'damage',
+  number,
+  'get'
+>(container.get<Weapon>('Weapon'), 'damage', 'get');
+
+container.unbind('Weapon');
+// End-example
