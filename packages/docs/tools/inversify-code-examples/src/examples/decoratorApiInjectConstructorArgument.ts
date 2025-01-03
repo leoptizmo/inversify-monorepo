87c59a308fd5bfe5b4bf037493a23b214245e03e
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2025-01-03 10:38:14 +0100

    feat(inversify-code-examples): add inject param decorator example

---
+++ packages/docs/tools/inversify-code-examples/src/examples/decoratorApiInjectConstructorArgument.ts
@@ -0,0 +1,34 @@
+import { Container, inject, injectable } from 'inversify';
+
+interface Weapon {
+  damage: number;
+}
+
+const weaponServiceId: symbol = Symbol.for('WeaponServiceId');
+
+@injectable()
+class Katana {
+  public readonly damage: number = 10;
+}
+
+// Begin-example
+@injectable()
+class Ninja {
+  constructor(
+    @inject(weaponServiceId)
+    public readonly weapon: Weapon,
+  ) {}
+}
+
+const container: Container = new Container();
+
+container.bind(Ninja).toSelf();
+container.bind(weaponServiceId).to(Katana);
+
+const ninja: Ninja = container.get(Ninja);
+
+// Returns 10
+const ninjaWeaponDamage: number = ninja.weapon.damage;
+// End-example
+
+export { ninjaWeaponDamage };
