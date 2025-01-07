eaba6b711e4e84e582025b242679d268e58f9e47
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2025-01-07 15:08:03 +0100

    refactor(core): add injectable

--- packages/container/libraries/core/src/metadata/decorators/injectable.ts
+++ packages/container/libraries/core/src/metadata/decorators/injectable.ts
@@ -2,12 +2,15 @@ import { updateReflectMetadata } from '@inversifyjs/reflect-metadata-utils';
 
 import { BindingScope } from '../../binding/models/BindingScope';
 import { classMetadataReflectKey } from '../../reflectMetadata/data/classMetadataReflectKey';
+import { updateClassMetadataWithTypescriptParameterTypes } from '../actions/updateClassMetadataWithTypescriptParameterTypes';
 import { getDefaultClassMetadata } from '../calculations/getDefaultClassMetadata';
 import { MaybeClassMetadata } from '../models/MaybeClassMetadata';
 
 export function injectable(scope?: BindingScope): ClassDecorator {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
   return (target: Function): void => {
+    updateClassMetadataWithTypescriptParameterTypes(target);
+
     if (scope !== undefined) {
       updateReflectMetadata<MaybeClassMetadata>(
         target,
