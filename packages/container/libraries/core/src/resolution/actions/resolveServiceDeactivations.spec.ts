086c1ff6ef7c9dae08891d11e6d091a5ca13871a
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2025-01-09 12:26:00 +0100

    refactor(core): update resolveServiceDeactivations to rely on resolveBindingsDeactivations

--- packages/container/libraries/core/src/resolution/actions/resolveServiceDeactivations.spec.ts
+++ packages/container/libraries/core/src/resolution/actions/resolveServiceDeactivations.spec.ts
@@ -2,12 +2,10 @@ import { afterAll, beforeAll, describe, expect, it, jest } from '@jest/globals';
 
 import { ServiceIdentifier } from '@inversifyjs/common';
 
-jest.mock('./resolveBindingDeactivations');
+jest.mock('./resolveBindingsDeactivations');
 
-import { ConstantValueBindingFixtures } from '../../binding/fixtures/ConstantValueBindingFixtures';
-import { ConstantValueBinding } from '../../binding/models/ConstantValueBinding';
 import { DeactivationParams } from '../models/DeactivationParams';
-import { resolveBindingDeactivations } from './resolveBindingDeactivations';
+import { resolveBindingsDeactivations } from './resolveBindingsDeactivations';
 import { resolveServiceDeactivations } from './resolveServiceDeactivations';
 
 describe(resolveServiceDeactivations.name, () => {
@@ -17,15 +15,13 @@ describe(resolveServiceDeactivations.name, () => {
   beforeAll(() => {
     paramsMock = {
       getBindings: jest.fn() as unknown,
-      getClassMetadata: jest.fn(),
-      getDeactivations: jest.fn(),
     } as Partial<
       jest.Mocked<DeactivationParams>
     > as jest.Mocked<DeactivationParams>;
     serviceIdentifierFixture = 'service-id';
   });
 
-  describe('when called, and params.getBindings() returns undefined', () => {
+  describe('when called', () => {
     let result: unknown;
 
     beforeAll(() => {
@@ -43,127 +39,19 @@ describe(resolveServiceDeactivations.name, () => {
       expect(paramsMock.getBindings).toHaveBeenCalledTimes(1);
       expect(paramsMock.getBindings).toHaveBeenCalledWith(
         serviceIdentifierFixture,
+      );
+    });
+
+    it('should call resolveBindingsDeactivations()', () => {
+      expect(resolveBindingsDeactivations).toHaveBeenCalledTimes(1);
+      expect(resolveBindingsDeactivations).toHaveBeenCalledWith(
+        paramsMock,
+        undefined,
       );
     });
 
     it('should return undefined', () => {
       expect(result).toBeUndefined();
-    });
-  });
-
-  describe('when called, and params.getBindings() returns an array with singleton ScopedBinding with no cached value', () => {
-    let result: unknown;
-
-    beforeAll(() => {
-      paramsMock.getBindings.mockReturnValueOnce([
-        ConstantValueBindingFixtures.withCacheWithIsRightFalse,
-      ]);
-
-      result = resolveServiceDeactivations(
-        paramsMock,
-        serviceIdentifierFixture,
-      );
-    });
-
-    afterAll(() => {
-      jest.clearAllMocks();
-    });
-
-    it('should call params.getBindings()', () => {
-      expect(paramsMock.getBindings).toHaveBeenCalledTimes(1);
-      expect(paramsMock.getBindings).toHaveBeenCalledWith(
-        serviceIdentifierFixture,
-      );
-    });
-
-    it('should return undefined', () => {
-      expect(result).toBeUndefined();
-    });
-  });
-
-  describe('when called, and params.getBindings() returns an array with non instance singleton ScopedBinding with cached value', () => {
-    let bindingFixture: ConstantValueBinding<unknown>;
-
-    let result: unknown;
-
-    beforeAll(() => {
-      bindingFixture = ConstantValueBindingFixtures.withCacheWithIsRightTrue;
-
-      paramsMock.getBindings.mockReturnValueOnce([bindingFixture]);
-
-      result = resolveServiceDeactivations(
-        paramsMock,
-        serviceIdentifierFixture,
-      );
-    });
-
-    afterAll(() => {
-      jest.clearAllMocks();
-    });
-
-    it('should call params.getBindings()', () => {
-      expect(paramsMock.getBindings).toHaveBeenCalledTimes(1);
-      expect(paramsMock.getBindings).toHaveBeenCalledWith(
-        serviceIdentifierFixture,
-      );
-    });
-
-    it('should call resolveBindingDeactivations()', () => {
-      expect(resolveBindingDeactivations).toHaveBeenCalledTimes(1);
-      expect(resolveBindingDeactivations).toHaveBeenCalledWith(
-        paramsMock,
-        bindingFixture,
-      );
-    });
-
-    it('should return undefined', () => {
-      expect(result).toBeUndefined();
-    });
-  });
-
-  describe('when called, and params.getBindings() returns an array with non instance singleton ScopedBinding with cached value and resolveBindingDeactivations() returns Promise', () => {
-    let bindingFixture: ConstantValueBinding<unknown>;
-
-    let result: unknown;
-
-    beforeAll(() => {
-      bindingFixture = ConstantValueBindingFixtures.withCacheWithIsRightTrue;
-
-      paramsMock.getBindings.mockReturnValueOnce([bindingFixture]);
-
-      (
-        resolveBindingDeactivations as jest.Mock<
-          typeof resolveBindingDeactivations
-        >
-      ).mockReturnValueOnce(Promise.resolve(undefined));
-
-      result = resolveServiceDeactivations(
-        paramsMock,
-        serviceIdentifierFixture,
-      );
-    });
-
-    afterAll(() => {
-      jest.clearAllMocks();
-    });
-
-    it('should call params.getBindings()', () => {
-      expect(paramsMock.getBindings).toHaveBeenCalledTimes(1);
-      expect(paramsMock.getBindings).toHaveBeenCalledWith(
-        serviceIdentifierFixture,
-      );
-    });
-
-    it('should call resolveBindingDeactivations()', () => {
-      expect(resolveBindingDeactivations).toHaveBeenCalledTimes(1);
-      expect(resolveBindingDeactivations).toHaveBeenCalledWith(
-        paramsMock,
-        bindingFixture,
-      );
-    });
-
-    it('should return undefined', () => {
-      expect(result).toStrictEqual(Promise.resolve(undefined));
     });
   });
 });
--- packages/container/libraries/core/src/resolution/actions/resolveServiceDeactivations.ts
+++ packages/container/libraries/core/src/resolution/actions/resolveServiceDeactivations.ts
@@ -1,24 +1,8 @@
-import { Right, ServiceIdentifier } from '@inversifyjs/common';
+import { ServiceIdentifier } from '@inversifyjs/common';
 
-import { isScopedBinding } from '../../binding/calculations/isScopedBinding';
 import { Binding } from '../../binding/models/Binding';
-import { bindingScopeValues } from '../../binding/models/BindingScope';
-import { BindingType } from '../../binding/models/BindingType';
-import { ScopedBinding } from '../../binding/models/ScopedBinding';
 import { DeactivationParams } from '../models/DeactivationParams';
-import { Resolved } from '../models/Resolved';
-import { resolveBindingDeactivations } from './resolveBindingDeactivations';
-
-const CACHE_KEY_TYPE: keyof ScopedBinding<
-  BindingType,
-  typeof bindingScopeValues.Singleton,
-  unknown
-> = 'cache';
-
-type CachedSingletonScopedBinding<TResolved> = Binding &
-  ScopedBinding<BindingType, typeof bindingScopeValues.Singleton, TResolved> & {
-    [CACHE_KEY_TYPE]: Right<Resolved<TResolved>>;
-  };
+import { resolveBindingsDeactivations } from './resolveBindingsDeactivations';
 
 export function resolveServiceDeactivations(
   params: DeactivationParams,
@@ -27,43 +11,5 @@ export function resolveServiceDeactivations(
   const bindings: Iterable<Binding> | undefined =
     params.getBindings(serviceIdentifier);
 
-  if (bindings === undefined) {
-    return;
-  }
-
-  const singletonScopedBindings: CachedSingletonScopedBinding<unknown>[] =
-    filterCachedSinglentonScopedBindings(bindings);
-
-  const deactivationPromiseResults: Promise<void>[] = [];
-
-  for (const binding of singletonScopedBindings) {
-    const deactivationResult: void | Promise<void> =
-      resolveBindingDeactivations(params, binding);
-
-    if (deactivationResult !== undefined) {
-      deactivationPromiseResults.push(deactivationResult);
-    }
-  }
-
-  if (deactivationPromiseResults.length > 0) {
-    return Promise.all(deactivationPromiseResults).then(() => undefined);
-  }
-}
-
-function filterCachedSinglentonScopedBindings(
-  bindings: Iterable<Binding>,
-): CachedSingletonScopedBinding<unknown>[] {
-  const filteredBindings: CachedSingletonScopedBinding<unknown>[] = [];
-
-  for (const binding of bindings) {
-    if (
-      isScopedBinding(binding) &&
-      binding.scope === bindingScopeValues.Singleton &&
-      binding.cache.isRight
-    ) {
-      filteredBindings.push(binding as CachedSingletonScopedBinding<unknown>);
-    }
-  }
-
-  return filteredBindings;
+  return resolveBindingsDeactivations(params, bindings);
 }
