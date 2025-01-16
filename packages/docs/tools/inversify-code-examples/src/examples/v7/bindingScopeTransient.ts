c88bc508289c8defbfa196b0db92d104e5068924
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2025-01-17 00:24:13 +0100

    feat(inversify-code-examples): add bindingScopeTransient example

---
+++ packages/docs/tools/inversify-code-examples/src/examples/v7/bindingScopeTransient.ts
@@ -0,0 +1,22 @@
+import { Container } from 'inversify7';
+
+interface Weapon {
+  damage: number;
+}
+
+export class Katana implements Weapon {
+  public readonly damage: number = 10;
+}
+
+// Begin-example
+const container: Container = new Container();
+container.bind<Weapon>('Weapon').to(Katana).inTransientScope();
+
+const firstKatana: Weapon = container.get<Weapon>('Weapon');
+const secondKatana: Weapon = container.get<Weapon>('Weapon');
+
+// Returns false
+const isSameKatana: boolean = firstKatana === secondKatana;
+// End-example
+
+export { isSameKatana };
