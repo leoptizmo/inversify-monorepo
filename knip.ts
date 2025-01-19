ef3826c6cb7c27ecc30db0bc98fccb480672338d
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2025-01-19 02:21:09 +0100

    chore: update ci to search for unused code

--- knip.ts
+++ knip.ts
@@ -4,11 +4,16 @@ type RecordValues<T> = T extends Record<any, infer U> ? U : never;
 
 type WorkspaceProjectConfig = RecordValues<Required<KnipConfig["workspaces"]>>;
 
-const defaultWorkspaceProjectConfig: WorkspaceProjectConfig = {
+const defaultWorkspaceProjectConfig: WorkspaceProjectConfig & {
+  entry: string[];
+  ignoreDependencies: string[];
+  project: string[];
+} = {
   entry: [
     "{index,cli,main}.{js,cjs,mjs,jsx,ts,cts,mts,tsx}",
     "src/{index,cli,main}.{js,cjs,mjs,jsx,ts,cts,mts,tsx}",
   ],
+  ignoreDependencies: ["ts-loader", "tslib"],
   project: [
     "**/*.{js,cjs,mjs,jsx,ts,cts,mts,tsx}!",
     "!jest.config.stryker.mjs",
@@ -17,31 +22,50 @@ const defaultWorkspaceProjectConfig: WorkspaceProjectConfig = {
 };
 
 export default {
+  commitlint: {
+    config: "config/commitlint/commitlint.config.js",
+  },
+  ignoreWorkspaces: ["packages/docs/services/inversify-site"],
   workspaces: {
     ".": {
-      entry: ["config/commitlint/commitlint.config.js"],
+      entry: [],
+      ignoreDependencies: defaultWorkspaceProjectConfig.ignoreDependencies,
       project: [],
     },
     "packages/container/libraries/*": defaultWorkspaceProjectConfig,
     "packages/container/tools/*": defaultWorkspaceProjectConfig,
     "packages/docs/services/*": defaultWorkspaceProjectConfig,
+    "packages/docs/tools/*": defaultWorkspaceProjectConfig,
     "packages/docs/tools/inversify-code-examples": {
       entry: ["src/examples/**/*.ts", "src/scripts/generateExamples.mts"],
+      ignoreDependencies: [
+        ...defaultWorkspaceProjectConfig.ignoreDependencies,
+        "@docusaurus/Link",
+        "@docusaurus/plugin-content-docs",
+        "@docusaurus/useDocusaurusContext",
+        "@theme/*",
+      ],
       project: defaultWorkspaceProjectConfig.project,
     },
-    "packages/docs/tools/*": defaultWorkspaceProjectConfig,
     "packages/foundation/libraries/*": defaultWorkspaceProjectConfig,
     "packages/foundation/tools/*": defaultWorkspaceProjectConfig,
+    "packages/foundation/tools/prettier-config": {
+      entry: ["{cjs,esm}/index.{js,d.ts}"],
+      ignoreDependencies: defaultWorkspaceProjectConfig.ignoreDependencies,
+      project: defaultWorkspaceProjectConfig.project,
+    },
     "packages/container/tools/e2e-tests": {
       entry: [
+        "config/*.mjs",
         "src/*/parameters/*.ts",
         "src/*/step-definitions/*.ts",
         "src/app/hooks/*.ts",
       ],
-      project: [
-        ...(defaultWorkspaceProjectConfig.project as string[]),
-        "!config/*",
+      ignoreDependencies: [
+        ...defaultWorkspaceProjectConfig.ignoreDependencies,
+        "ts-node",
       ],
+      project: [...defaultWorkspaceProjectConfig.project, "!config/*"],
     },
   },
 } satisfies KnipConfig;
