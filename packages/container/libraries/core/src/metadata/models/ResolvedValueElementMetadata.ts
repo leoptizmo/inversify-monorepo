7ac2c28cde4d816046885b5a5399a53974adf20f
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2025-02-01 11:04:36 +0100

    refactor(core): add ResolvedValueMetadata

---
+++ packages/container/libraries/core/src/metadata/models/ResolvedValueElementMetadata.ts
@@ -0,0 +1,17 @@
+import { LazyServiceIdentifier, ServiceIdentifier } from '@inversifyjs/common';
+
+import { BaseResolvedValueElementMetadata } from './BaseResolvedValueElementMetadata';
+import { MetadataName } from './MetadataName';
+import { MetadataTag } from './MetadataTag';
+import { ResolvedValueElementMetadataKind } from './ResolvedValueElementMetadataKind';
+
+export interface ResolvedValueElementMetadata
+  extends BaseResolvedValueElementMetadata<
+    | ResolvedValueElementMetadataKind.singleInjection
+    | ResolvedValueElementMetadataKind.multipleInjection
+  > {
+  name: MetadataName | undefined;
+  optional: boolean;
+  tags: Map<MetadataTag, unknown>;
+  value: ServiceIdentifier | LazyServiceIdentifier;
+}
