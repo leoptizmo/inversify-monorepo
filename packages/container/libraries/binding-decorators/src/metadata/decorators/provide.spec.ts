aa5427b9d961795c60c3535e8ee1f09b521524f2
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2025-02-07 21:05:25 +0100

    feat(binding-decorator): update provide with optional service id

--- packages/container/libraries/binding-decorators/src/metadata/decorators/provide.spec.ts
+++ packages/container/libraries/binding-decorators/src/metadata/decorators/provide.spec.ts
@@ -1,10 +1,14 @@
-import { beforeAll, describe, expect, it, jest } from '@jest/globals';
+import { afterAll, beforeAll, describe, expect, it, jest } from '@jest/globals';
 
 jest.mock('@inversifyjs/reflect-metadata-utils');
 jest.mock('../actions/updateMetadataMap');
 
 import { updateOwnReflectMetadata } from '@inversifyjs/reflect-metadata-utils';
-import { BindInWhenOnFluentSyntax, BindToFluentSyntax } from 'inversify';
+import {
+  BindInWhenOnFluentSyntax,
+  BindToFluentSyntax,
+  ServiceIdentifier,
+} from 'inversify';
 
 import { bindingMetadataMapReflectKey } from '../../reflectMetadata/data/bindingMetadataMapReflectKey';
 import { updateMetadataMap } from '../actions/updateMetadataMap';
@@ -13,10 +17,76 @@ import { BindingMetadataMap } from '../models/BindingMetadataMap';
 import { provide } from './provide';
 
 describe(provide.name, () => {
-  describe('having no binding', () => {
+  describe('having no service identifier', () => {
     let bindInWhenOnFluentSyntaxFixture: BindInWhenOnFluentSyntax<unknown>;
     let bindToFluentSyntaxMock: jest.Mocked<BindToFluentSyntax<unknown>>;
-    let serviceIdentifierFixture: symbol;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+    let targetFixture: Function;
+    let updateMetadataMapResultFixture: (
+      bindingMetadataMap: BindingMetadataMap,
+    ) => BindingMetadataMap;
+
+    beforeAll(() => {
+      bindInWhenOnFluentSyntaxFixture =
+        Symbol() as unknown as BindInWhenOnFluentSyntax<unknown>;
+      bindToFluentSyntaxMock = {
+        to: jest.fn(),
+      } as Partial<jest.Mocked<BindToFluentSyntax<unknown>>> as jest.Mocked<
+        BindToFluentSyntax<unknown>
+      >;
+      targetFixture = class {};
+      updateMetadataMapResultFixture = Symbol() as unknown as (
+        bindingMetadataMap: BindingMetadataMap,
+      ) => BindingMetadataMap;
+    });
+
+    describe('when called', () => {
+      let result: unknown;
+
+      beforeAll(() => {
+        bindToFluentSyntaxMock.to.mockReturnValueOnce(
+          bindInWhenOnFluentSyntaxFixture,
+        );
+
+        (
+          updateMetadataMap as jest.Mock<typeof updateMetadataMap>
+        ).mockReturnValueOnce(updateMetadataMapResultFixture);
+
+        result = provide()(targetFixture);
+      });
+
+      afterAll(() => {
+        jest.clearAllMocks();
+      });
+
+      it('should call updateMetadataMap()', () => {
+        expect(updateMetadataMap).toHaveBeenCalledTimes(1);
+        expect(updateMetadataMap).toHaveBeenCalledWith(targetFixture, {
+          action: expect.any(Function),
+          serviceIdentifier: targetFixture,
+        });
+      });
+
+      it('should call updateOwnReflectMetadata()', () => {
+        expect(updateOwnReflectMetadata).toHaveBeenCalledTimes(1);
+        expect(updateOwnReflectMetadata).toHaveBeenCalledWith(
+          Object,
+          bindingMetadataMapReflectKey,
+          buildDefaultBindingMetadataMap,
+          updateMetadataMapResultFixture,
+        );
+      });
+
+      it('should return undefined', () => {
+        expect(result).toBeUndefined();
+      });
+    });
+  });
+
+  describe('having service identifier', () => {
+    let bindInWhenOnFluentSyntaxFixture: BindInWhenOnFluentSyntax<unknown>;
+    let bindToFluentSyntaxMock: jest.Mocked<BindToFluentSyntax<unknown>>;
+    let serviceIdentifierFixture: ServiceIdentifier;
     // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
     let targetFixture: Function;
     let updateMetadataMapResultFixture: (
@@ -51,6 +121,10 @@ describe(provide.name, () => {
         ).mockReturnValueOnce(updateMetadataMapResultFixture);
 
         result = provide(serviceIdentifierFixture)(targetFixture);
+      });
+
+      afterAll(() => {
+        jest.clearAllMocks();
       });
 
       it('should call updateMetadataMap()', () => {
