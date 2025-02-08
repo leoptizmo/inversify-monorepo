9ac503af0ec77982ec820a57ad1666b377a1985b
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2025-02-08 21:51:20 +0100

    fix(container): update Container.restore
    update restore flow to recalculate computed properties

--- packages/container/libraries/container/src/container/services/Container.int.spec.ts
+++ packages/container/libraries/container/src/container/services/Container.int.spec.ts
@@ -19,182 +19,293 @@ class Arsenal {
 }
 
 describe(Container.name, () => {
-  describe('when container has one gun binding and one arsenal binding', () => {
-    describe('when container.get is called for Arsenal', () => {
-      let container: Container;
+  describe('.get', () => {
+    describe('when container has one gun binding and one arsenal binding', () => {
+      describe('when container.get is called for Arsenal', () => {
+        let container: Container;
 
-      let arsenal: Arsenal;
+        let arsenal: Arsenal;
 
-      beforeAll(() => {
-        container = new Container();
+        beforeAll(() => {
+          container = new Container();
 
-        container.bind(Gun).to(Gun);
-        container.bind(Arsenal).to(Arsenal);
+          container.bind(Gun).to(Gun);
+          container.bind(Arsenal).to(Arsenal);
 
-        arsenal = container.get(Arsenal);
+          arsenal = container.get(Arsenal);
+        });
+
+        it('should provide an arsenal with two guns', () => {
+          expect(arsenal).toBeInstanceOf(Arsenal);
+          expect(arsenal.guns).toHaveLength(1);
+          arsenal.guns.forEach((gun: Gun) => expect(gun).toBeInstanceOf(Gun));
+        });
       });
 
-      it('should provide an arsenal with two guns', () => {
-        expect(arsenal).toBeInstanceOf(Arsenal);
-        expect(arsenal.guns).toHaveLength(1);
-        arsenal.guns.forEach((gun: Gun) => expect(gun).toBeInstanceOf(Gun));
-      });
-    });
+      describe('when container.get is called twice and gun binding is unbound in the middle', () => {
+        let container: Container;
+        let arsenal: Arsenal;
 
-    describe('when container.get is called twice and gun binding is unbound in the middle', () => {
-      let container: Container;
-      let arsenal: Arsenal;
+        beforeAll(async () => {
+          container = new Container();
 
-      beforeAll(async () => {
-        container = new Container();
+          container.bind(Gun).to(Gun);
+          container.bind(Arsenal).to(Arsenal);
 
-        container.bind(Gun).to(Gun);
-        container.bind(Arsenal).to(Arsenal);
-
-        // First call to get Arsenal
-        container.get(Arsenal);
-
-        // Unbind Gun
-        await container.unbind(Gun);
-
-        // Second call to get Arsenal
-        arsenal = container.get(Arsenal);
-      });
-
-      it('should provide an arsenal with no guns', () => {
-        expect(arsenal).toBeInstanceOf(Arsenal);
-        expect(arsenal.guns).toHaveLength(0);
-      });
-    });
-
-    describe('when container.get is called twice and all bindings are unbound in the middle', () => {
-      let container: Container;
-      let result: unknown;
-
-      beforeAll(async () => {
-        container = new Container();
-
-        container.bind(Gun).to(Gun);
-        container.bind(Arsenal).to(Arsenal);
-
-        // First call to get Arsenal
-        container.get(Arsenal);
-
-        // Unbind all bindings
-        await container.unbindAll();
-
-        // Second call to get Arsenal
-        try {
+          // First call to get Arsenal
           container.get(Arsenal);
-        } catch (error: unknown) {
-          result = error;
-        }
+
+          // Unbind Gun
+          await container.unbind(Gun);
+
+          // Second call to get Arsenal
+          arsenal = container.get(Arsenal);
+        });
+
+        it('should provide an arsenal with no guns', () => {
+          expect(arsenal).toBeInstanceOf(Arsenal);
+          expect(arsenal.guns).toHaveLength(0);
+        });
       });
 
-      it('should throw an Error', () => {
-        const expectedErrorProperties: Partial<Error> = {
-          message: `No bindings found for service: "Arsenal".
+      describe('when container.get is called twice and all bindings are unbound in the middle', () => {
+        let container: Container;
+        let result: unknown;
+
+        beforeAll(async () => {
+          container = new Container();
+
+          container.bind(Gun).to(Gun);
+          container.bind(Arsenal).to(Arsenal);
+
+          // First call to get Arsenal
+          container.get(Arsenal);
+
+          // Unbind all bindings
+          await container.unbindAll();
+
+          // Second call to get Arsenal
+          try {
+            container.get(Arsenal);
+          } catch (error: unknown) {
+            result = error;
+          }
+        });
+
+        it('should throw an Error', () => {
+          const expectedErrorProperties: Partial<Error> = {
+            message: `No bindings found for service: "Arsenal".
 
 Trying to resolve bindings for "Arsenal (Root service)".
 
 Binding constraints:
 - service identifier: Arsenal
 - name: -`,
-        };
+          };
 
-        expect(result).toBeInstanceOf(Error);
-        expect(result).toMatchObject(expectedErrorProperties);
+          expect(result).toBeInstanceOf(Error);
+          expect(result).toMatchObject(expectedErrorProperties);
+        });
+      });
+
+      describe('when container.get is called twice and gun binding is added in the middle', () => {
+        let container: Container;
+        let arsenal: Arsenal;
+
+        beforeAll(() => {
+          container = new Container();
+
+          container.bind(Gun).to(Gun);
+          container.bind(Arsenal).to(Arsenal);
+
+          // First call to get Arsenal
+          container.get(Arsenal);
+
+          // Add Gun binding
+          container.bind(Gun).to(Gun);
+
+          // Second call to get Arsenal
+          arsenal = container.get(Arsenal);
+        });
+
+        it('should provide an arsenal with one gun', () => {
+          expect(arsenal).toBeInstanceOf(Arsenal);
+          expect(arsenal.guns).toHaveLength(2);
+          arsenal.guns.forEach((gun: Gun) => expect(gun).toBeInstanceOf(Gun));
+        });
+      });
+
+      describe('when container.get is called twice and gun binding is unbound using a ContainerModule in the middle', () => {
+        let container: Container;
+        let arsenal: Arsenal;
+
+        beforeAll(async () => {
+          container = new Container();
+
+          container.bind(Gun).to(Gun);
+          container.bind(Arsenal).to(Arsenal);
+
+          // First call to get Arsenal
+          container.get(Arsenal);
+
+          // Create and load a ContainerModule to unbind Gun
+          const module: ContainerModule = new ContainerModule(
+            async ({ unbind }: ContainerModuleLoadOptions) => {
+              await unbind(Gun);
+            },
+          );
+
+          await container.load(module);
+
+          // Second call to get Arsenal
+          arsenal = container.get(Arsenal);
+        });
+
+        it('should provide an arsenal with no guns', () => {
+          expect(arsenal).toBeInstanceOf(Arsenal);
+          expect(arsenal.guns).toHaveLength(0);
+        });
+      });
+
+      describe('when container.get is called twice and gun binding is added using a ContainerModule in the middle', () => {
+        let container: Container;
+        let arsenal: Arsenal;
+
+        beforeAll(async () => {
+          container = new Container();
+
+          container.bind(Arsenal).to(Arsenal);
+
+          // First call to get Arsenal
+          container.get(Arsenal);
+
+          // Create and load a ContainerModule to bind Gun
+          const module: ContainerModule = new ContainerModule(
+            ({ bind }: ContainerModuleLoadOptions) => {
+              bind(Gun).to(Gun);
+            },
+          );
+
+          await container.load(module);
+
+          // Second call to get Arsenal
+          arsenal = container.get(Arsenal);
+        });
+
+        it('should provide an arsenal with one gun', () => {
+          expect(arsenal).toBeInstanceOf(Arsenal);
+          expect(arsenal.guns).toHaveLength(1);
+          arsenal.guns.forEach((gun: Gun) => expect(gun).toBeInstanceOf(Gun));
+        });
       });
     });
+  });
 
-    describe('when container.get is called twice and gun binding is added in the middle', () => {
+  describe('.snapshot', () => {
+    describe('having some classes to be bound', () => {
       let container: Container;
-      let arsenal: Arsenal;
+
+      @injectable()
+      class Ninja {}
+
+      @injectable()
+      class Samurai {}
 
       beforeAll(() => {
         container = new Container();
-
-        container.bind(Gun).to(Gun);
-        container.bind(Arsenal).to(Arsenal);
-
-        // First call to get Arsenal
-        container.get(Arsenal);
-
-        // Add Gun binding
-        container.bind(Gun).to(Gun);
-
-        // Second call to get Arsenal
-        arsenal = container.get(Arsenal);
       });
 
-      it('should provide an arsenal with one gun', () => {
-        expect(arsenal).toBeInstanceOf(Arsenal);
-        expect(arsenal.guns).toHaveLength(2);
-        arsenal.guns.forEach((gun: Gun) => expect(gun).toBeInstanceOf(Gun));
-      });
-    });
+      describe('when classes are bound', () => {
+        beforeAll(() => {
+          container.bind(Ninja).to(Ninja);
+          container.bind(Samurai).to(Samurai);
+        });
 
-    describe('when container.get is called twice and gun binding is unbound using a ContainerModule in the middle', () => {
-      let container: Container;
-      let arsenal: Arsenal;
+        it('should be able to resolve the classes', () => {
+          expect(container.get(Ninja)).toBeInstanceOf(Ninja);
+          expect(container.get(Samurai)).toBeInstanceOf(Samurai);
+        });
 
-      beforeAll(async () => {
-        container = new Container();
+        describe('when called container.snapshot() and Ninja is unbound', () => {
+          beforeAll(async () => {
+            container.snapshot();
+            await container.unbind(Ninja);
+          });
 
-        container.bind(Gun).to(Gun);
-        container.bind(Arsenal).to(Arsenal);
+          it('should not be able to resolve Ninja', () => {
+            expect(() => container.get(Ninja)).toThrow();
+          });
 
-        // First call to get Arsenal
-        container.get(Arsenal);
+          it('should be able to resolve Samurai', () => {
+            expect(container.get(Samurai)).toBeInstanceOf(Samurai);
+          });
 
-        // Create and load a ContainerModule to unbind Gun
-        const module: ContainerModule = new ContainerModule(
-          async ({ unbind }: ContainerModuleLoadOptions) => {
-            await unbind(Gun);
-          },
-        );
+          describe('when called container.snapshot()', () => {
+            beforeAll(async () => {
+              container.snapshot();
+            });
 
-        await container.load(module);
+            it('should not be able to resolve Ninja', () => {
+              expect(() => container.get(Ninja)).toThrow();
+            });
 
-        // Second call to get Arsenal
-        arsenal = container.get(Arsenal);
-      });
+            it('should be able to resolve Samurai', () => {
+              expect(container.get(Samurai)).toBeInstanceOf(Samurai);
+            });
 
-      it('should provide an arsenal with no guns', () => {
-        expect(arsenal).toBeInstanceOf(Arsenal);
-        expect(arsenal.guns).toHaveLength(0);
-      });
-    });
+            describe('when Ninja is bound again', () => {
+              beforeAll(() => {
+                container.bind(Ninja).to(Ninja);
+              });
 
-    describe('when container.get is called twice and gun binding is added using a ContainerModule in the middle', () => {
-      let container: Container;
-      let arsenal: Arsenal;
+              it('should be able to resolve the classes', () => {
+                expect(container.get(Ninja)).toBeInstanceOf(Ninja);
+                expect(container.get(Samurai)).toBeInstanceOf(Samurai);
+              });
 
-      beforeAll(async () => {
-        container = new Container();
+              describe('when called container.restore()', () => {
+                beforeAll(() => {
+                  container.restore();
+                });
 
-        container.bind(Arsenal).to(Arsenal);
+                it('should not be able to resolve Ninja', () => {
+                  expect(() => container.get(Ninja)).toThrow();
+                });
 
-        // First call to get Arsenal
-        container.get(Arsenal);
+                it('should be able to resolve Samurai', () => {
+                  expect(container.get(Samurai)).toBeInstanceOf(Samurai);
+                });
 
-        // Create and load a ContainerModule to bind Gun
-        const module: ContainerModule = new ContainerModule(
-          ({ bind }: ContainerModuleLoadOptions) => {
-            bind(Gun).to(Gun);
-          },
-        );
+                describe('when called container.restore()', () => {
+                  beforeAll(() => {
+                    container.restore();
+                  });
 
-        await container.load(module);
+                  it('should be able to resolve the classes', () => {
+                    expect(container.get(Ninja)).toBeInstanceOf(Ninja);
+                    expect(container.get(Samurai)).toBeInstanceOf(Samurai);
+                  });
 
-        // Second call to get Arsenal
-        arsenal = container.get(Arsenal);
-      });
+                  describe('when called container.restore()', () => {
+                    let result: unknown;
 
-      it('should provide an arsenal with one gun', () => {
-        expect(arsenal).toBeInstanceOf(Arsenal);
-        expect(arsenal.guns).toHaveLength(1);
-        arsenal.guns.forEach((gun: Gun) => expect(gun).toBeInstanceOf(Gun));
+                    beforeAll(() => {
+                      try {
+                        container.restore();
+                      } catch (error: unknown) {
+                        result = error;
+                      }
+                    });
+
+                    it('should throw an Error', () => {
+                      expect(result).toBeInstanceOf(Error);
+                    });
+                  });
+                });
+              });
+            });
+          });
+        });
       });
     });
   });
