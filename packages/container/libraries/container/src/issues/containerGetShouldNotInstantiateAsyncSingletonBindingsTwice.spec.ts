3f4d8609b8e410c764b650b67b1467cd750c7b4d
Roberto Pintos López <roberto.pintos.lopez@gmail.com>
2025-02-27 19:20:41 +0100

    test(container): add regression test

---
+++ packages/container/libraries/container/src/issues/containerGetShouldNotInstantiateAsyncSingletonBindingsTwice.spec.ts
@@ -0,0 +1,63 @@
+import { beforeAll, describe, expect, it } from '@jest/globals';
+
+import 'reflect-metadata';
+
+import { inject, injectable } from '@inversifyjs/core';
+
+import { Container } from '../container/services/Container';
+
+class AsyncDependency {
+  public static async create(): Promise<AsyncDependency> {
+    return new AsyncDependency();
+  }
+}
+
+@injectable()
+class Dependency1 {
+  constructor(
+    @inject(AsyncDependency)
+    public readonly asyncDependency: AsyncDependency,
+  ) {}
+}
+
+@injectable()
+class Dependency2 {
+  constructor(
+    @inject(AsyncDependency) public readonly asyncDependency: AsyncDependency,
+  ) {}
+}
+
+@injectable()
+class Application {
+  constructor(
+    @inject(Dependency1)
+    public readonly dependency1: Dependency1,
+    @inject(Dependency2)
+    public readonly dependency2: Dependency2,
+  ) {}
+}
+
+describe('Container.get should not instantiate async singleton bindings twice', () => {
+  let result: unknown;
+
+  beforeAll(async () => {
+    const container: Container = new Container({ defaultScope: 'Singleton' });
+
+    container.bind(Application).toSelf();
+    container.bind(Dependency1).toSelf();
+    container.bind(Dependency2).toSelf();
+    container
+      .bind(AsyncDependency)
+      .toDynamicValue(async () => AsyncDependency.create());
+
+    result = await container.getAsync(Application);
+  });
+
+  it('should return expected value', () => {
+    expect(result).toBeInstanceOf(Application);
+
+    expect((result as Application).dependency1.asyncDependency).toBe(
+      (result as Application).dependency2.asyncDependency,
+    );
+  });
+});
