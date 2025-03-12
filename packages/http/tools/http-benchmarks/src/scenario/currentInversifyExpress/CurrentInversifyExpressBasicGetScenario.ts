c013726364f898d60a866d1acf78c9276932b351
Adrianmjim <adrianmjim199434@gmail.com>
2025-03-12 12:31:58 +0100

    feat(http-benchmarks): CurrentInversifyExpressBasicGetScenario

---
+++ packages/http/tools/http-benchmarks/src/scenario/currentInversifyExpress/CurrentInversifyExpressBasicGetScenario.ts
@@ -0,0 +1,40 @@
+import { controller, GET } from '@inversifyjs/http-core';
+import { InversifyExpressHttpAdapter } from '@inversifyjs/http-express';
+import { Container } from 'inversify';
+
+import { CurrentInversifyExpressBaseScenario } from './CurrentInversifyExpressBaseScenario';
+
+@controller()
+class AppController {
+  @GET()
+  public ok(): string {
+    return 'ok';
+  }
+}
+
+export class CurrentInversifyExpressBasicGetScenario extends CurrentInversifyExpressBaseScenario {
+  public override async execute(): Promise<void> {
+    try {
+      await fetch(`http://localhost:${String(this._port)}`);
+    } catch {
+      void 0;
+    }
+  }
+
+  public override async setUp(): Promise<void> {
+    const container: Container = new Container({ defaultScope: 'Singleton' });
+
+    container.bind(AppController).toSelf();
+
+    const server: InversifyExpressHttpAdapter = new InversifyExpressHttpAdapter(
+      container,
+      {
+        logger: false,
+      },
+    );
+
+    this._app = server.build();
+
+    this._server = this._app.listen(this._port);
+  }
+}
