f051bc5077e01ef4e9b5f11bffd3111b30422dd3
Adrianmjim <adrianmjim199434@gmail.com>
2025-03-14 13:40:52 +0100

    chore(http-benchmarks): remove currentInversifyFastify platform

--- packages/http/tools/http-benchmarks/src/scenario/models/Platform.ts
+++ packages/http/tools/http-benchmarks/src/scenario/models/Platform.ts
@@ -1,7 +1,6 @@
 export enum Platform {
   inversify6Express = 'inversify6Express',
   currentInversifyExpress = 'currentInversifyExpress',
-  currentInversifyFastify = 'currentInversifyFastify',
   express = 'express',
   fastify = 'fastify',
   nestJsExpress = 'NestJSExpress',
