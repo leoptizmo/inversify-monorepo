40f3e2698fdcc985151cd810769530e2cc400a0f
Adrianmjim <adrianmjim199434@gmail.com>
2025-03-27 14:34:25 +0100

    fix(http-express): update _getCookies method to include response parameter

--- packages/http/libraries/express/src/adapter/InversifyExpressHttpAdapter.ts
+++ packages/http/libraries/express/src/adapter/InversifyExpressHttpAdapter.ts
@@ -149,7 +149,11 @@ export class InversifyExpressHttpAdapter extends InversifyHttpAdapter<
       : request.headers;
   }
 
-  protected _getCookies(request: Request, parameterName?: string): unknown {
+  protected _getCookies(
+    request: Request,
+    _response: Response,
+    parameterName?: string,
+  ): unknown {
     return parameterName !== undefined
       ? (request.cookies as Record<string, unknown>)[parameterName]
       : request.cookies;
