dd2eea0081d268205274c04ce3dfe37a36fd5fe8
Adrianmjim <adrianmjim199434@gmail.com>
2025-03-27 12:15:52 +0100

    refactor(http-core): remove next decorator

--- packages/http/libraries/core/src/http/decorators/Next.ts
+++
@@ -1,6 +0,0 @@
-import { RequestMethodParameterType } from '../models/RequestMethodParameterType';
-import { requestParam } from './RequestParam';
-
-export function next(): ParameterDecorator {
-  return requestParam(RequestMethodParameterType.NEXT);
-}
