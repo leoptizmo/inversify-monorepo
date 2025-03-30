a444e6093600aedd12e42459b82c6d871cab2d88
Adrianmjim <adrianmjim199434@gmail.com>
2025-03-30 13:45:22 +0200

    refactor(http-benchmarks): refactor HonoBasicGetScenario to use K6

--- packages/http/tools/http-benchmarks/src/scenario/hono/HonoBasicGetScenario.ts
+++ packages/http/tools/http-benchmarks/src/scenario/hono/HonoBasicGetScenario.ts
@@ -1,25 +1,12 @@
-import { serve } from '@hono/node-server';
-import { Context } from 'hono';
+import { BaseK6Scenario } from '../k6/BaseK6Scenario';
+import { Platform } from '../models/Platform';
 
-import { HonoBaseScenario } from './HonoBaseScenario';
-
-export class HonoBasicGetScenario extends HonoBaseScenario {
-  public async execute(): Promise<void> {
-    try {
-      await fetch(`http://localhost:${String(this._port)}`);
-    } catch {
-      void 0;
-    }
-  }
-
-  public async setUp(): Promise<void> {
-    this._app.get('/', (ctx: Context) => {
-      return ctx.text('ok');
-    });
-
-    this._server = serve({
-      fetch: this._app.fetch,
-      port: this._port,
-    });
+export class HonoBasicGetScenario extends BaseK6Scenario {
+  constructor() {
+    super(
+      Platform.hono,
+      'BasicGetScenario.ts',
+      'hono/setUpHonoBasicGetScenario.js',
+    );
   }
 }
