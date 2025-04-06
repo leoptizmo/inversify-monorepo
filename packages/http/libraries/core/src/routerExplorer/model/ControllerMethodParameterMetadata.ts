aa4d44355cdc77f6c0bd02641c23efb88e8d5899
Adrianmjim <adrianmjim199434@gmail.com>
2025-04-06 12:28:41 +0200

    feat(http-core): add customParameterDecoratorHandler to ControllerMethodParameterMetadata interface

--- packages/http/libraries/core/src/routerExplorer/model/ControllerMethodParameterMetadata.ts
+++ packages/http/libraries/core/src/routerExplorer/model/ControllerMethodParameterMetadata.ts
@@ -1,6 +1,8 @@
+import { CustomParameterDecoratorHandler } from '../../http/models/CustomParameterDecoratorHandler';
 import { RequestMethodParameterType } from '../../http/models/RequestMethodParameterType';
 
 export interface ControllerMethodParameterMetadata {
+  customParameterDecoratorHandler?: CustomParameterDecoratorHandler | undefined;
   index: number;
   parameterType: RequestMethodParameterType;
   parameterName?: string | undefined;
