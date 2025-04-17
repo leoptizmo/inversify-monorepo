ae344d33f29b4d69799affc4205acbfb6e44ad55
Adrianmjim <adrianmjim199434@gmail.com>
2025-04-17 10:18:05 +0200

    fix(http-core): update isPipe function to handle undefined and null values correctly

--- packages/http/libraries/core/src/http/typeguard/isPipe.spec.ts
+++ packages/http/libraries/core/src/http/typeguard/isPipe.spec.ts
@@ -4,7 +4,77 @@ import { Pipe } from '../pipe/model/Pipe';
 import { isPipe } from './isPipe';
 
 describe(isPipe.name, () => {
-  describe('having a value that is Pipe', () => {
+  describe('having a value undefined', () => {
+    describe('when called', () => {
+      let valueFixture: unknown;
+      let result: boolean;
+
+      beforeAll(() => {
+        valueFixture = undefined;
+
+        result = isPipe(valueFixture);
+      });
+
+      it('should return false', () => {
+        expect(result).toBe(false);
+      });
+    });
+  });
+
+  describe('having a value null', () => {
+    describe('when called', () => {
+      let valueFixture: null;
+      let result: boolean;
+
+      beforeAll(() => {
+        valueFixture = null;
+
+        result = isPipe(valueFixture);
+      });
+
+      it('should return false', () => {
+        expect(result).toBe(false);
+      });
+    });
+  });
+
+  describe('having a value with no execute property', () => {
+    describe('when called', () => {
+      let valueFixture: object;
+      let result: boolean;
+
+      beforeAll(() => {
+        valueFixture = {};
+
+        result = isPipe(valueFixture);
+      });
+
+      it('should return false', () => {
+        expect(result).toBe(false);
+      });
+    });
+  });
+
+  describe('having a value with non function execute property', () => {
+    describe('when called', () => {
+      let valueFixture: { execute: string };
+      let result: boolean;
+
+      beforeAll(() => {
+        valueFixture = {
+          execute: 'not a function',
+        };
+
+        result = isPipe(valueFixture);
+      });
+
+      it('should return false', () => {
+        expect(result).toBe(false);
+      });
+    });
+  });
+
+  describe('having a value Pipe', () => {
     describe('when called', () => {
       let valueFixture: Pipe;
       let result: boolean;
@@ -19,23 +89,6 @@ describe(isPipe.name, () => {
 
       it('should return true', () => {
         expect(result).toBe(true);
-      });
-    });
-  });
-
-  describe('having a value that is not Pipe', () => {
-    describe('when called', () => {
-      let valueFixture: unknown;
-      let result: boolean;
-
-      beforeAll(() => {
-        valueFixture = undefined;
-
-        result = isPipe(valueFixture);
-      });
-
-      it('should return false', () => {
-        expect(result).toBe(false);
       });
     });
   });
--- packages/http/libraries/core/src/http/typeguard/isPipe.ts
+++ packages/http/libraries/core/src/http/typeguard/isPipe.ts
@@ -1,10 +1,11 @@
 import { Pipe } from '../pipe/model/Pipe';
 
 export function isPipe(value: unknown): value is Pipe {
-  const pipe: Pipe = value as Pipe;
+  const pipe: Partial<Pipe> = value as Partial<Pipe>;
 
   return (
     value !== undefined &&
+    value !== null &&
     typeof pipe === 'object' &&
     typeof pipe.execute === 'function'
   );
